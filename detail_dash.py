09abb2e28db4893e9ab4d4e022f808bf6e3fe558
Russ-Moonride <140208843+Russ-Moonride@users.noreply.github.com>
2025-02-12 19:53:01 -0500

    Update detail_dash.py

--- detail_dash.py
+++ detail_dash.py
@@ -106,11 +106,12 @@ def main():
     # Load Hubspot data
     hs_data = load_hs_data()
 
+    st.write(merged_data)
+    st.write(merged_data.columns)
+
     merged_data = pd.merge(meta_data, meta_ref_data, on="Ad Name", how="left")
     merged_data = pd.merge(merged_data, meta_camp_data, on="Campaign Name", how="left")
-    merged_data = pd.merge(merged_data, hs_data, on="Campaign Name", how="left")
-
-    st.write(merged_data)
+    #merged_data = pd.merge(merged_data, hs_data, on="Campaign Name", how="left")
     
     ### **Add Campaign Type filter**
     type_options = ["All"] + sorted(merged_data["Type"].dropna().astype(str).unique().tolist()) + ["Unmapped"]
